19808eb19a559d1d746ee5607cadbd17421d58d7
RafaMiyauchi <rafamiyauchi@gmail.com>
2025-11-05 14:05:53 +0700

    Finished with Tutorial 7

---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:football_news/screens/menu.dart';
+// Import the newslist_form.dart page
+import 'package:football_news/screens/newslist_form.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            // Drawer header section
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Football News',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "All the latest football updates here!",
+                  // Add text style with center alignment, font size 15, white
+                  // color, and normal weight
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Routing section
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Redirect to MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.post_add),
+            title: const Text('Add News'),
+            // Redirect to NewsFormPage
+            onTap: () {
+              // Add routing to NewsFormPage here
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const NewsFormPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
