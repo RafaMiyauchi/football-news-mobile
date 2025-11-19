9d145f82e39ec2537690f7cf66da0e8c2f96ceb6
RafaMiyauchi <rafamiyauchi@gmail.com>
2025-11-19 13:38:02 +0700

    Pushing Triggering Bitrise

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:football_news/screens/login.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
+// My application main.dart
 void main() {
   runApp(const MyApp());
 }
